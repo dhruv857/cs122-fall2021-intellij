fa64f040b2d147f5a422c076bda48bb0d6c77240
dhruv857 <dgandhi@pace.edu>
2021-09-15 01:22:47 -0400

    initial

---
+++ src/classlab/week2/Animal.java
@@ -0,0 +1,10 @@
+
+
+public class Animal {
+//    String noise
+//    Integer legs
+    String noise;
+    int legs;
+
+//
+}
---
+++ src/classlab/week2/Dog.java
@@ -0,0 +1,6 @@
+
+
+public class Dog extends Animal {
+    int eyes;
+//    Integer eye
+}
